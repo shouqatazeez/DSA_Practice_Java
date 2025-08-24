ff13875623174a8117005438e8a213b1df7933ff
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-24 07:08:12 +0530

    find the max value among the array elements

---
+++ Maxvalue.java
@@ -0,0 +1,22 @@
+public class Maxvalue {
+    public static void main(String[] args) {
+
+        int[] arr = { 100, 45, 2700, 12, 38 };
+        System.out.println(maxvalue(arr));
+
+    }
+
+    static int maxvalue(int[] arr) {
+        int max = arr[0];// here we consider the arr[0] as the max value
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {// then after we check whether the array values in the array is greater thant
+                               // the max value
+
+                max = arr[i];
+
+            }
+        }
+
+        return max;
+    }
+}
